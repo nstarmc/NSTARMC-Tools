858aab798aaa4aec097f094d32b4d83e9ce12c34
luluxiaoyu <ljy.xiaoyu@qq.com>
2022-02-08 11:05:51 +0800

    完成Java下载页面获取版本列表

--- NSTARMC-Tools/pages/java.xaml.vb
+++ NSTARMC-Tools/pages/java.xaml.vb
@@ -1,3 +1,49 @@
-﻿Class java
+﻿Imports System.Threading
+Imports ModernWpf.Controls
+
+Class java
+    Private Sub Page_Loaded_1(sender As Object, e As RoutedEventArgs)
+        Dim homepagestart As Thread = New Thread(AddressOf Homepagestartthread)
+        homepagestart.Start()
+    End Sub
+    Private Function Homepagestartthread(ByVal objParamReport As Object) As String
+        mclist.Dispatcher.Invoke(New Action(Sub()
+                                                mclist.Items.Clear()
+                                            End Sub))
+
+
+        For Each dirlist In System.IO.Directory.GetDirectories(My.Application.Info.DirectoryPath & "\file\")
+            If My.Computer.FileSystem.FileExists(dirlist & "\info.xml") Then
+
+                '检测到文件夹内为符合格式的整合包，加载
+                Dim info_reader As String = My.Computer.FileSystem.ReadAllText(dirlist & "\info.xml")
+                Dim info_xml As XElement = XElement.Parse(info_reader)
+                mclist.Dispatcher.Invoke(New Action(Sub()
+                                                        mclist.Items.Add(info_xml.<mcversion>.Value & " - " & info_xml.<modloader>.Value & "(" & info_xml.<sharder>.Value & ")")
+
+                                                    End Sub))
+            End If
+        Next
+        If mclist.Items.Count = 0 Then
+            mclist.Dispatcher.Invoke(New Action(Sub()
+                                                    mclist.IsEnabled = False
+                                                    Dim dialog As ContentDialog = New ContentDialog() With {
+                                                        .Title = "错误",
+                                                        .CloseButtonText = "我知道了",
+                                                        .IsPrimaryButtonEnabled = False,
+                                                        .DefaultButton = ContentDialogButton.Close,
+                                                        .Content = "无法在工具下的File检测到有效格式的整合包！" & vbCrLf & "请正确放置整合包文件后，重启工具再试！" & vbCrLf & "如还是无法检测，请在Q群求助！"
+                                                    }
+                                                    dialog.ShowAsync()
+                                                End Sub))
+
+        Else
+            mclist.Dispatcher.Invoke(New Action(Sub()
+                                                    mclist.SelectedIndex = 0
+                                                End Sub))
+
+        End If
+    End Function
+
 
 End Class
